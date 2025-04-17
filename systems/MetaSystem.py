d73984c90ada2449682802381f7b33b09b6ad33d
Louiszk <133251643+Louiszk@users.noreply.github.com>
2025-04-17 08:45:50 +0200

    Update MetaSystem.py

--- systems/MetaSystem.py
+++ systems/MetaSystem.py
@@ -158,7 +158,7 @@ def build_system():
     # Node: MetaAgent
     # Description: Meta Agent
     def meta_agent_function(state: Dict[str, Any]) -> Dict[str, Any]:  
-        llm = LargeLanguageModel(temperature=1, wrapper="google", model_name="gemini-2.0-flash")
+        llm = LargeLanguageModel(temperature=0.2, wrapper="google", model_name="gemini-2.0-flash")
         
         context_length = 8*2 # even
         messages = state.get("messages", [])
